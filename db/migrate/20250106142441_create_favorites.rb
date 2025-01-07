fbfd8381b087f7d9eecd516b033ad396ade281b7
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2025-01-07 05:58:24 +0000

    ouyou3

--- db/migrate/20250106142441_create_favorites.rb
+++ db/migrate/20250106142441_create_favorites.rb
@@ -2,7 +2,7 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
       t.integer :user_id
-      t.string :post_image_integer
+      t.integer :book_id
 
       t.timestamps
     end
